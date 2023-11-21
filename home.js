eb64371681be6d0c25a8104c4ec8cbe61828f675
Ruth <ruthshachar234@gmail.com>
2023-11-21 13:15:05 +0200

    my project

---
+++ home.js
@@ -0,0 +1,3 @@
+ 
+document.getElementById("bot").addEventListener("click", function() { window.location.href = "file:///C:/Users/User/Documents/%D7%AA%D7%A8%D7%92%D7%95%D7%9C%20fullstak/%D7%A4%D7%A8%D7%95%D7%99%D7%A7%D7%98/home.html"; });
+
